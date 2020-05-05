88785df55ca27511fcf92e7b6e3c6c95c74260ba
Santhosh Kumar <santhosh@anattadesign.com>
2020-05-05 14:12:51 +0530

    Dice 1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" ref="dC" v-on="listeners">
+    <HelloWorld :animate="animate" />
+    <HelloWorld :animate="animate" />
+    <!-- <HelloWorld /> -->
   </div>
 </template>
 
@@ -12,6 +13,91 @@ export default {
   name: "App",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      x: Number,
+      y: Number,
+      limit: Number,
+      currentLimit: Number,
+      isDrawing: Number,
+      show: true,
+      rotateValue: 0,
+      rotateDeg: 0,
+      animate: { x: 0, y: 0, x1: 0, y1: 0, rotateDeg: 0 }
+    };
+  },
+  computed: {
+    listeners() {
+      return {
+        touchstart: this.mouseDown,
+        touchend: this.touchUp,
+        touchmove: this.mouseMove,
+        mousedown: this.mouseDown,
+        mousemove: this.mouseMove,
+        mouseup: this.mouseUp
+      };
+    }
+  },
+  methods: {
+    mouseDown(e) {
+      if (e instanceof TouchEvent) {
+        const touchobj = e.changedTouches[0];
+        this.x = touchobj.pageX;
+        this.y = touchobj.pageY;
+        this.isDrawing = true;
+      } else {
+        this.x = e.offsetX;
+        this.y = e.offsetY;
+        this.isDrawing = true;
+      }
+    },
+    mouseMove(e) {
+      if (e instanceof TouchEvent) {
+        // const touchobj = e.changedTouches[0];
+        if (this.isDrawing === true) {
+          this.currentLimit = 0;
+        }
+      } else {
+        if (this.isDrawing === true) {
+          this.currentLimit = e.offsetX;
+        }
+      }
+    },
+    mouseUp(e) {
+      if (this.isDrawing === true) {
+        // console.log(this.swipeTigger(e.offsetX, e.offsetY));
+        this.animate = { x: this.x, y: this.y, x1: e.offsetX, y1: e.offsetY };
+        this.x = 0;
+        this.y = 0;
+        this.currentLimit = 0;
+        this.isDrawing = false;
+      }
+    },
+    touchUp(e) {
+      if (e instanceof TouchEvent) {
+        const touchobj = e.changedTouches[0];
+        if (this.isDrawing === true) {
+          // console.log(this.swipeTigger(e.offsetX, e.offsetY));
+          const dist = Math.abs(touchobj.pageX - this.x); // get total dist traveled by finger while in contact with surface // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+          // elapsedTime = new Date().getTime() - startTime
+          // get time elapsed
+          var swiperightBol = dist >= 150;
+          if (swiperightBol) {
+            this.animate = {
+              x: this.x,
+              y: this.y,
+              x1: touchobj.pageX,
+              y1: touchobj.pageY
+            };
+          }
+          this.x = 0;
+          this.y = 0;
+          this.currentLimit = 0;
+          this.isDrawing = false;
+        }
+      }
+    }
   }
 };
 </script>
@@ -23,6 +109,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+body {
+  margin: 0px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,334 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <!-- <main> -->
+  <div class="dice-container">
+    <!-- <textarea v-model="rotateValue" />
+    {{ classObject }} -->
+    <transition v-on:before-enter="beforeEnter" v-on:enter="enter">
+      <div class="dice" :style="classObject">
+        <div class="front diceLayer">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div class="back diceLayer">
+          <!-- <span></span> -->
+        </div>
+        <div class="right diceLayer">
+          <span></span>
+        </div>
+        <div class="left diceLayer">
+          <span></span>
+          <span></span>
+        </div>
+        <div class="top diceLayer">
+          <!-- <span></span>
+          <span></span>
+          <span></span> -->
+        </div>
+        <div class="bottom diceLayer">
+          <!-- <span></span>
+          <span></span>
+          <span></span>
+          <span></span> -->
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
+<style lang="scss">
+.dice-container {
+  margin: 60px;
+}
+
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.add-keyframe {
+  animation-name: rotate;
+  animation-iteration-count: 1;
+  animation-duration: 3s;
+  animation-timing-function: ease-in-out;
+  animation-name: opacity;
+}
+.diceLayer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+.dice {
+  width: 20px;
+  height: 200px;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: rotateX(0deg) rotateY(3600deg) translateX(0);
+  animation-iteration-count: 1;
+  //  animation-name: rotate;
+  animation-duration: 1s;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
+  transform-origin: top center;
+
+  > div {
+    height: 200px;
+    width: 20px;
+    position: absolute;
+    background: #f76331;
+    border-radius: 2px;
+    border: 1px solid #f77d54;
+    padding: 1px;
+  }
+
+  span {
+    width: 8px;
+    height: 8px;
+    background: #fff;
+    border-radius: 50%;
+    display: block;
+    // position: absolute;
+  }
+
+  .front {
+    transform: rotateY(0deg) translateZ(10px);
+    span {
+      &:nth-child(1) {
+        top: 10px;
+        left: 12px;
+      }
+      &:nth-child(2) {
+        top: 10px;
+        right: 12px;
+      }
+      &:nth-child(3) {
+        top: 26px;
+        left: 12px;
+      }
+      &:nth-child(4) {
+        top: 26px;
+        right: 12px;
+      }
+      &:nth-child(5) {
+        bottom: 10px;
+        left: 12px;
+      }
+      &:nth-child(6) {
+        bottom: 10px;
+        right: 12px;
+      }
+    }
+  }
+  .back {
+    transform: rotateX(180deg) translateZ(10px);
+
+    span {
+      top: 26px;
+      left: 26px;
+    }
+  }
+  .right {
+    transform: rotateY(90deg) translateZ(10px);
+
+    span {
+      &:nth-child(1) {
+        top: 12px;
+        left: 12px;
+      }
+      &:nth-child(2) {
+        top: 12px;
+        right: 12px;
+      }
+      &:nth-child(3) {
+        top: 26px;
+        left: 26px;
+      }
+      &:nth-child(4) {
+        bottom: 12px;
+        left: 12px;
+      }
+      &:nth-child(5) {
+        bottom: 12px;
+        right: 12px;
+      }
+    }
+  }
+  .left {
+    transform: rotateY(-90deg) translateZ(10px);
+
+    span {
+      &:nth-child(1) {
+        top: 12px;
+        right: 12px;
+      }
+      &:nth-child(2) {
+        bottom: 12px;
+        left: 12px;
+      }
+    }
+  }
+  .top {
+    transform: rotateX(90deg) translateZ(10px);
+    height: 20px;
+    span {
+      &:nth-child(1) {
+        top: 12px;
+        right: 12px;
+      }
+      &:nth-child(2) {
+        bottom: 12px;
+        left: 12px;
+      }
+      &:nth-child(3) {
+        bottom: 26px;
+        left: 26px;
+      }
+    }
+  }
+  .bottom {
+    transform: rotateX(-90deg) translateZ(192px);
+    height: 20px;
+    span {
+      &:nth-child(1) {
+        top: 12px;
+        right: 12px;
+      }
+      &:nth-child(2) {
+        top: 12px;
+        left: 12px;
+      }
+      &:nth-child(3) {
+        bottom: 12px;
+        left: 12px;
+      }
+      &:nth-child(4) {
+        bottom: 12px;
+        right: 12px;
+      }
+    }
+  }
+}
+
+.button {
+  position: fixed;
+  bottom: 20px;
+  background: #f76939;
+  padding: 20px 40px;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background: #f76939 - 20;
+  }
+}
+
+@keyframes opacity {
+  0% {
+    opacity: 1;
+  }
+  20% {
+    opacity: 0.5;
+  }
+  40% {
+    opacity: 1;
+  }
+  60% {
+    opacity: 1;
+  }
+  80% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotateY(0deg) translateX(0px);
+  }
+  //  35% { transform: rotateX(-180deg) rotateY(-90deg) rotateZ(0deg) translateX(0); }
+  //  45% { transform: rotateX(-180deg) rotateY(-90deg) rotateZ(0deg) translateX(0); }
+  //  65% { transform: rotateX(0deg) rotateY(0deg) rotateZ(-360deg) translateX(0); }
+  //  75% { transform: rotateX(0deg) rotateY(0deg) rotateZ(-360deg) translateX(0); }
+  100% {
+    transform: rotateY(18000deg) translateX(0px);
+    transition-delay: 1s;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
 <script>
 export default {
-  name: "HelloWorld",
+  name: "Dice",
   props: {
-    msg: String
+    animate: { type: Object, default: () => ({}) }
+  },
+  data() {
+    return {
+      x: Number,
+      y: Number,
+      limit: Number,
+      currentLimit: Number,
+      isDrawing: Number,
+      show: true,
+      rotateValue: 0,
+      rotateDeg: 0
+    };
+  },
+  methods: {
+    beforeEnter: function() {
+      console.log("beforenter");
+    },
+    enter: function() {
+      console.log("enter");
+    },
+    swipeTigger(animate) {
+      let dx = animate.x - animate.x1;
+      let dy = animate.y - animate.y1;
+
+      const radians = Math.atan2(dy, dx);
+      var pi = Math.PI;
+      this.rotateDeg = radians * (180 / pi);
+      return this.rotateDeg;
+    },
+    showDice() {
+      this.show = !this.show;
+    }
+  },
+  watch: {
+    animate(val) {
+      this.swipeTigger(val);
+    }
+  },
+  computed: {
+    classObject() {
+      return {
+        transform:
+          "rotateZ(" +
+          (this.rotateDeg / 10) * Math.random() * 1 +
+          "deg) rotateY(" +
+          Math.ceil(Math.random() * 10) * 90 +
+          "deg) translateX(" +
+          Math.random() * 50 +
+          "px) translateY(" +
+          Math.random() * 50 +
+          "px)",
+        transition: `transform 0.3s`
+      };
+    }
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
